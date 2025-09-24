54aaad72b73cb291ead85458114a7c9317d6a064
Banakh Andrei Maksimovich <uuetsukeu@mail.ru>
2025-09-24 21:38:41 +0300

    feat: (lexer) token.h commets

--- src/lexer/token.h
+++ src/lexer/token.h
@@ -1,52 +1,65 @@
 #pragma once
 #include <string>
-#include <vector>
 
 enum class TokenType {
-    // Keywords
-    
-    // TYPES
+
+    // 1. Keywords
+
+    // 1.1 TYPES
     KW_INT, KW_BOOL, KW_LONG, KW_ARRAY,
-    // Special constants
+
+    // 1.2 Special constants
     KW_NONE, KW_TRUE, KW_FALSE
-    // Control flow
+
+    // 1.3 Control flow
     KW_IF, KW_ELSE, KW_ELIF, 
     KW_WHILE, KW_FOR, KW_BREAK, 
     KW_CONTINUE, KW_RETURN,
 
-    // structs, object.attr
+    // 1.4 Structs, object.attr
     KW_STRUCT, KW_DOT,
     
-    // Identifiers
+    // 2. Identifiers
     IDENTIFIER,
     
-    // Literals
-    INT_LITERAL, BOOL_LITERAL, LONG_LITERAL
+    // 3. Literals
+    INT_LITERAL, BOOL_LITERAL, LONG_LITERAL,
     
-    // Operators
-    // +-/*%
+    // 4. Operators
+
+    // 4.1. + - / * %
     OP_PLUS, OP_MINUS, OP_MULT, OP_DIV, OP_MOD,
-    // =
-    OP_ASSIGN, 
-    // == != <=>
+
+    // 4.2. = += -= /= *= %=
+    OP_ASSIGN, OP_ASSIGN_PLUS, OP_ASSIGN_MINUS, OP_ASSIGN_MULT, OP_ASSIGN_DIV, OP_ASSIGN_MOD,
+
+    // 4.3. == != <=>
     OP_EQ, OP_NE, OP_LT, OP_GT, OP_LE, OP_GE,
-    // and or not
+
+    // 4.4. and or not
     OP_AND, OP_OR, OP_NOT,
     
-    // Delimiters
-    // { } ( ) [ ]
+    // 5. Delimiters
+
+    // 5.1. { } ( ) [ ]
     LPAREN, RPAREN, LBRACE, RBRACE, LBRACKET, RBRACKET,
-    // ; ,
+
+    // 5.2 ; ,
     SEMICOLON, COMMA,
     
-    // Special
+    // 6. Special
     END_OF_FILE, ERROR, // idk what is it
     
 };
 
+
+// This struct represents based indivisible brick of code written by.
+// value is used to storage text inside for diagnostics and future trouble-shootings
+// examples:
+// int x = 5 + 2 => [Token(TokenType(KW_INT), 'int'), Token(TokenType(IDENTIFIER), 'x'), Token(TokenType(OP_ASSIGN), '+') ....]
 struct Token {
     TokenType type;
     std::string value;
-    int line;
-    int column;
+    int line; // used for showing in lexical exceptions
+    int column; // used for showing in lexical exceptions
 };
